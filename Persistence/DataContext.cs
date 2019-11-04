592408520ddc2126b1648cc5cb526c179dbbe9bc
Andrea Perdono <andrea.perdono@regione.marche.it>
2019-11-04 16:36:57 +0100

    Commit Iniziale

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public DbSet<Value> Values { get; set; }
+
+        protected  override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "Valore 101"},
+                    new Value { Id = 2, Name = "Valore 102"},
+                    new Value { Id = 3, Name = "Valore 103"}
+                );
+        }
+    }
+}
