c68b9d8cc528a8275649b5ae5ce15dc8c66a19db
Andrea Perdono <andrea.perdono@regione.marche.it>
2019-11-14 19:17:12 +0100

    End 61

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,12 @@
-import React, {useState, useEffect}from 'react';
+import React, {useState, useEffect, Fragment}from 'react';
 import logo from './logo.svg';
 
 
 import axios from 'axios';
-import { Header, Icon, List } from 'semantic-ui-react'
+import { Header, Icon, List, Container } from 'semantic-ui-react'
 import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
+import ActivityDasboard from '../../features/activities/dashboard/ActivityDasboard';
 
 interface IState{
   activities: IActivity[];
@@ -12,6 +14,11 @@ interface IState{
 
 const App = () =>{
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelecetedActivity] = useState<IActivity | null>(null);
+
+  const handleSelectActivity = (id: string) =>{
+    setSelecetedActivity(activities.filter(a => a.id === id)[0])
+  }
 
   useEffect(()=>{
     axios.get<IActivity[]>('http://localhost:5000/api/activities').then((response)=>{
@@ -20,31 +27,18 @@ const App = () =>{
       });
     }, []);
   
-  //readonly state: IState = {
-    // activities: [],
-  //}
-
-  //componentDidMount() {
-    //axios.get<IActivity[]>('http://localhost:5000/api/activities').then((response)=>{
-     // console.log(response);
-      //this.setState({
-        //activities: response.data
-      //})
-    //})
-  //}
-  //render(){
     return (
-      <div>
-         <Header as='h2'>
-          <Icon name='plug' />
-            <Header.Content>Uptime Guarantee</Header.Content>
-        </Header>
-        <List>
-        {activities.map((activity)=>(
-              <List.Item key ={activity.id}>{activity.title}</List.Item>
-            ))}
-        </List>         
-      </div>
+      <Fragment>
+        <NavBar/>
+        <Container style={{marginTop: '7em'}}>
+          <List>
+            <ActivityDasboard 
+            activities={activities} 
+            selectActivity={handleSelectActivity}
+            selectedActivity={selectedActivity}/>
+          </List>       
+        </Container>
+      </Fragment>
     );
   //}
 }
