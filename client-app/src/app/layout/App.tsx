5d8c8c007833ec0dfc8e2666708bbd83109207ec
Andrea Perdono <andrea.perdono@regione.marche.it>
2019-11-18 19:00:22 +0100

    Errore corretto

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, Fragment}from 'react';
+import React, {useState, useEffect, Fragment, SyntheticEvent}from 'react';
 
 import {List, Container } from 'semantic-ui-react'
 import { IActivity } from '../models/activity';
@@ -16,6 +16,8 @@ const App = () =>{
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
+  const [target, setTarget] = useState('');
 
 
   const handleSelectActivity = (id: string) =>{
@@ -30,32 +32,39 @@ const App = () =>{
 
   //Gestisce la creazione di una Nuova attività
   const handleCreateActivity = (activity: IActivity) => { 
+    setSubmitting(true);
     agent.Activities.create(activity).then(()=> {
       setActivities([...activities, activity])
       setSelectedActivity(activity);
       setEditMode(false);
-    }).catch(function (error) {
+    }).then(() => setSubmitting(false)).catch(function (error) {
       console.log(error);
   });
   }
 
   //Gestisce la modifica dell'attività selezionata
   const handleEditActivity = (activity: IActivity) => { 
+    setSubmitting(true);
     agent.Activities.update(activity).then(()=> {
     setActivities([...activities.filter(a => a.id !== activity.id), activity])
     setSelectedActivity(activity);
     setEditMode(false);
     })
-  .catch(function (error) {
+    .then(() => setSubmitting(false))
+    .catch(function (error) {
       console.log(error);
   });
   }
 
   //Gestisce l'eliminazione della Attività selezionata
-  const handleDeleteActivity = (id: string) => {
+  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>,id: string) => {
+    setSubmitting(true);
+    setTarget(event.currentTarget.name);
     agent.Activities.delete(id).then(()=> {
     setActivities([...activities.filter(a => a.id !== id)])
-    }).catch(function (error) {
+    })
+    .then(() => setSubmitting(false))
+    .catch(function (error) {
       console.log(error);
   })
   }
@@ -88,7 +97,9 @@ const App = () =>{
             setSelectedActivity={setSelectedActivity}
             createActivity={handleCreateActivity}
             editActivity={handleEditActivity}
-            deleteActivity={handleDeleteActivity}/>
+            deleteActivity={handleDeleteActivity}
+            submitting = {submitting}
+            target = {target}/>
           </List>       
         </Container> 
       </Fragment>
